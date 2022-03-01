947255f19412170775a6c0c1bc16bed7991fd9a5
Nhi Tuyet Nguyen <83228558+snoween@users.noreply.github.com>
2022-03-01 18:28:27 +0800

    Update my_book_list.py

--- HW1/my_book_list.py
+++ HW1/my_book_list.py
@@ -1,4 +1,4 @@
 import book
-ds_book = book.Book('nhi xinh dep', 'hoc hanh nhu db', 'nhi', 'nam nay') 
+ds_book = book.Book('Python資料結構', '黃建庭', '全華圖書', '2021') 
 book_info = ds_book.info()  
 print(book_info)
